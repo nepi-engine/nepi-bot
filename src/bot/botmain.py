99a48afcccf01974876870b30dbed225ec1ed3a5
Andy Varella <avarella@consultant.com>
2021-02-16 10:50:25 -0500

    commented out multithreading until it can be reworked and tested.

--- src/bot/botmain.py
+++ src/bot/botmain.py
@@ -167,12 +167,14 @@ sm = BotMsg(cfg, log, db, 1)
 # Instantiate a HB transfer process if requested
 ########################################################################
 
-if nepi_args.hb is True:
-    hbproc = bothbproc.HbProc(cfg, log, 0, dev_id_str, nepi_args)
-#     log.track(1, "Starting HB thread worker process.", True)
-#     hb_thread = threading.Thread(target=hbproc.run_hb_proc)
-#     hb_thread.start()
-    success = hbproc.run_hb_proc()
+# if nepi_args.hb is True:
+hbproc = bothbproc.HbProc(cfg, log, 0, dev_id_str, nepi_args)
+rc = hbproc.run_hb_proc()
+exit()
+# #     log.track(1, "Starting HB thread worker process.", True)
+# #     hb_thread = threading.Thread(target=hbproc.run_hb_proc)
+# #     hb_thread.start()
+#     success = hbproc.run_hb_proc()
 
 ########################################################################
 # Re-Evaluate PIPO Ratings for Archived Data Products if Required.
