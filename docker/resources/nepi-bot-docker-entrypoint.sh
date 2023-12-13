14761e76fb149dec46051fde63fd97999225a605
Josh Maximoff <jmaximoff@numurus.com>
2023-12-13 11:46:09 -0500

    Add license headers and LICENSE text file
    Exclude 3rd-party files that are separately licensed and submodules that have their own generate_license_headers.sh script

--- docker/resources/nepi-bot-docker-entrypoint.sh
+++ docker/resources/nepi-bot-docker-entrypoint.sh
@@ -1,4 +1,29 @@
 #!/bin/ash
+##
+## NEPI Dual-Use License
+## Project: nepi-bot
+##
+## This license applies to any user of NEPI Engine software
+##
+## Copyright (C) 2023 Numurus, LLC <https://www.numurus.com>
+## see https://github.com/numurus-nepi/nepi-bot
+##
+## This software is dual-licensed under the terms of either a NEPI software developer license
+## or a NEPI software commercial license.
+##
+## The terms of both the NEPI software developer and commercial licenses
+## can be found at: www.numurus.com/licensing-nepi-engine
+##
+## Redistributions in source code must retain this top-level comment block.
+## Plagiarizing this software to sidestep the license obligations is illegal.
+##
+## Contact Information:
+## ====================
+## - https://www.numurus.com/licensing-nepi-engine
+## - mailto:nepi@numurus.com
+##
+##
+
 # Wait for a little bit before changing run-levels to let the container fully start
 sleep 3
 
--- utilities/change_identity.py
+++ utilities/change_identity.py
@@ -1,4 +1,28 @@
 #!/usr/bin/env python3
+#
+# NEPI Dual-Use License
+# Project: nepi-bot
+#
+# This license applies to any user of NEPI Engine software
+#
+# Copyright (C) 2023 Numurus, LLC <https://www.numurus.com>
+# see https://github.com/numurus-nepi/nepi-bot
+#
+# This software is dual-licensed under the terms of either a NEPI software developer license
+# or a NEPI software commercial license.
+#
+# The terms of both the NEPI software developer and commercial licenses
+# can be found at: www.numurus.com/licensing-nepi-engine
+#
+# Redistributions in source code must retain this top-level comment block.
+# Plagiarizing this software to sidestep the license obligations is illegal.
+#
+# Contact Information:
+# ====================
+# - https://www.numurus.com/licensing-nepi-engine
+# - mailto:nepi@numurus.com
+#
+#
 
 """Change the NUID and SSH keys for a given bot installation
 
