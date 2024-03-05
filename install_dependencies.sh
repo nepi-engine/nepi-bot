8d27583a528ef7bcd86f165c57e48a3714bdc4ee
Josh Maximoff <jmaximoff@numurus.com>
2024-03-05 13:17:21 -0500

    License updates - universal BSD-3-Clause for nepi-engine

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
+##
+## Copyright (c) 2024 Numurus, LLC <https://www.numurus.com>.
+##
+## This file is part of nepi-engine
+## (see https://github.com/nepi-engine).
+##
+## License: 3-clause BSD, see https://opensource.org/licenses/BSD-3-Clause
+##
 
 APT_INSTALLS="software-properties-common protobuf-compiler cmake python3-pip"
 PIP_INSTALLS="virtualenv"
